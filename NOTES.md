546587bbf4a59d5fb85cdaa0390424dab5f430b9
kawa2019 <kamil.ku@vp.pl>
2020-06-18 20:19:15 +0200

    sight for mobile devices

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,5 @@
 -w html dodałem vieport
 -json server zainstalowałem paczkę ,przepuściłem przez validator oraz całą bazę dałem w object
 -dodanie przykładowego kanału z css tak aby poznać jaki html będzie potrzebny by zrobić kilka kanałów
+
+stworzenie widoku dla 544px szerokości, dodanie przecinków w liczbach, 
--- static/app.js
+++ static/app.js
@@ -1,10 +1,40 @@
 const mainContent = document.querySelector(".wrapper__main");
+const numberWithCommas = (x) => {
+    if (typeof x !== "undefined")
+        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 
+const html = (link_to_channel, logo, channel_title, num_sub, num_vid, num_view) => {
+
+    return (`
+<a href=${link_to_channel} class="logo" style="background-image: url(${logo})">
+</a>
+<h2 class="channel_title">${channel_title}</h2>
+<div class="all_numbers">
+    <p class="numbers">
+        <span class="sub_vid_view">SUBSCRIBERS:</span>
+        <span class="num_sub_vid_view">${numberWithCommas(num_sub)}</span>
+    </p>
+    <p class="numbers">
+        <span class="sub_vid_view">VIDEOS:</span>
+        <span class="num_sub_vid_view">${numberWithCommas(num_vid)}</span>
+    </p>
+    <p class="numbers">
+        <span class="sub_vid_view">VIEWS:</span>
+        <span class="num_sub_vid_view">${numberWithCommas(num_view)}</span>
+    </p>
+</div>
+`)
+}
+console.log(html().trim())
 const one_channel = (data) => {
     data.map(channel => {
-        const html=""
-        const newArt = document.createElement("article");
+        const param_html = [channel.customUrl, channel.thumbnails.default.url, channel.title, channel.statistics.subscriberCount,
+        channel.statistics.videoCount, channel.statistics.viewCount]
+        let newArt = document.createElement("article");
+        newArt.classList.add("channel")
+        newArt.innerHTML = html(...param_html)
         mainContent.appendChild(newArt)
     })
 }
@@ -14,10 +44,8 @@ const getChannels = async () => {
         const response = await fetch("http://localhost:4000/channels");
         if (response.ok) {
             const data = await response.json();
-            data.map(channel => {
-                const newArt = document.createElement("article");
-                mainContent.appendChild(newArt);
-            })
+            console.log(data)
+            one_channel(data)
         } else {
             throw ("404 not found")
         }
@@ -28,10 +56,4 @@ const getChannels = async () => {
         mainContent.innerText = "Problem is " + error
     }
 }
-//getChannels()
-// function htmlToElement(html) {
-//     var template = document.createElement('template');
-//     html = html.trim(); // Never return a text node of whitespace as the result
-//     template.innerHTML = html;
-//     return template.content.firstChild;
-// }
+getChannels()
